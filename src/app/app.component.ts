d3be15eb6d8b9ea742e510d2921b4a9892c0be76
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-01-17 17:11:26 +0100

    Registration and Login working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -13,6 +14,11 @@ export class AppComponent {
   constructor(public dialog: MatDialog) { }
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     });
   }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userCredentials = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userCredentials).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('user', JSON.stringify(response.user));
+      console.log(response);
+      console.log('token', response.token);
+      this.snackBar.open('user logged in', 'OK', { duration: 500 });
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', { duration: 500 });
+    });
+  }
+
+}
